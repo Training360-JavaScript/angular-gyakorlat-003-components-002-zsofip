8938109fd28c5318470c037809687bec18074bf7
zsofip <91950899+zsofip@users.noreply.github.com>
2022-01-10 22:56:18 +0100

    done

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -14,7 +14,7 @@ export class UserDetailComponent implements OnInit {
    * @var user {User} - Input tulajdonság.
    * @default új User
    */
-
+  @Input() user: User = new User();
 
   @Output() delUser: EventEmitter<User> = new EventEmitter();
 
@@ -31,6 +31,8 @@ export class UserDetailComponent implements OnInit {
    * @param user {User} - az aktuális felhasználó.
    * @returns {void}
    */
-
+  onDelete(user: User): void {
+    this.delUser.emit(user);
+  }
 
 }
